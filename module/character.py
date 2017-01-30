f3901ec0a66b7fe2d6a2f5870e978c7b674642f2
raboliotthegreat <raboliotthegreat@mailoo.org>
2017-01-30 12:43:16 +0100

    add Character class

--- module/character.py
+++ module/character.py
@@ -1,2 +1,8 @@
 import pygame;
 import objet;
+
+class Character(Objet):
+    def __init__(self, pos_x, pos_y, imgPath, hp, speed):
+        Object.__init__(self, pos_x, pos_y, imgPath)
+        self.hp = hp;
+        self.speed = vitesse;
