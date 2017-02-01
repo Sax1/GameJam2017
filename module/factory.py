7cacc9458eddde6f4dba0a743ec4f40e246f4cd2
KAY-MORAND Guillaume <kaymorag@hostname.iut2.upmf-grenoble.fr>
2017-02-01 09:28:27 +0100

    bouboule vs 2 player

--- module/factory.py
+++ module/factory.py
@@ -7,5 +7,5 @@ import projectile
 def listeboulefeu():
     listboulefeu = [];
     for i in range(0,100):
-        listboulefeu.append(projectile.Projectile(-100,-100,'./data/minibouboule.png',40,(0,0),"feu",-10,0))
+        listboulefeu.append(projectile.Projectile(-100,-100,'./data/minibouboule.png',20,(0,0),"feu",-10,0))
     return listboulefeu
--- module/util.py
+++ module/util.py
@@ -15,50 +15,88 @@ def displayAllImages(window, listElements):
         window.blit(elem.get_img(),elem.get_pos());
 
 def actionForKeyInput(keys, listPlayers, playerDeplacement, nbJoueur, listDecors, listSorts):
+    touchJ1(keys,listPlayers[0],listDecors, playerDeplacement, listSorts)
+    if(nbJoueur == 2):
+        touchJ2(keys,listPlayers[1],listDecors, playerDeplacement, listSorts)
+
+def touchJ1(keys, player, listDecors, playerDeplacement, listSorts):
     if(keys[122]):
-        if(listPlayers[0].getJump() == 0 and listPlayers[0].getLanded() == 1):
-            listPlayers[0].setJump(1);
-            listPlayers[0].setAscend(1);
-            listPlayers[0].setAscendValue(10);
-            listPlayers[0].setDeplacement(0,-listPlayers[0].getAscendValue());
+        if(player.getJump() == 0 and player.getLanded() == 1):
+            player.setJump(1);
+            player.setAscend(1);
+            player.setAscendValue(10);
+            player.setDeplacement(0,-player.getAscendValue());
     if(keys[115]):
         print '1bas';
         #sys.exit();
     if( keys[113]):
-        if(physic.isTouching('left',listPlayers[0],listDecors) == 0):
-            listPlayers[0].setDeplacement(-playerDeplacement,0)
-            listPlayers[0].set_image("./sprite/Joueur1/MageGA-1.png")
+        if(physic.isTouching('left',player,listDecors) == 0):
+            player.setDeplacement(-playerDeplacement,0)
+            player.set_image("./sprite/Joueur1/MageGA-1.png")
     if( keys[100]):
-        if(physic.isTouching('rigth',listPlayers[0],listDecors) == 0):
-            listPlayers[0].setDeplacement(playerDeplacement,0)
+        if(physic.isTouching('right',player,listDecors) == 0):
+            player.setDeplacement(playerDeplacement,0)
     if(keys[106] and keys[121]):
         print("haut droit")
+        magic.launchBoule((1,-1),listSorts, player)
     elif(keys[106] and keys[104]):
         print("bas droit")
+        magic.launchBoule((1,1),listSorts, player)
     elif(keys[106]):#droite
-        magic.launchBoule(listSorts, listPlayers[0])
+        magic.launchBoule((1,0),listSorts, player)
     elif(keys[103] and keys[104]):
         print("bas gauche")
+        magic.launchBoule((-1,1),listSorts, player)
     elif(keys[103] and keys[121]):
         print("haut gauche")
+        magic.launchBoule((-1,-1),listSorts, player)
     elif(keys[103]):#gauche
         print("gauche")
+        magic.launchBoule((-1,0),listSorts, player)
     elif(keys[104]):#bas
         print("bas")
+        magic.launchBoule((0,1),listSorts, player)
     elif(keys[121]):#haut
         print("haut")
-    if(nbJoueur == 2):
-        if(keys[K_UP]):
-            if(listPlayers[1].getJump() == 0 and listPlayers[1].getLanded() == 1):
-                listPlayers[1].setJump(1);
-                listPlayers[1].setAscend(1);
-                listPlayers[1].setAscendValue(10);
-                listPlayers[1].setDeplacement(0,-listPlayers[1].getAscendValue());
-        if(keys[K_DOWN]):
-            print '2bas';
-        if(keys[K_LEFT]):
-            listPlayers[1].setDeplacement(-playerDeplacement,0)
-            listPlayers[1].set_image("./sprite/Joueur2/Mage2GA-1.png")
-        if(keys[K_RIGHT]):
-            listPlayers[1].setDeplacement(playerDeplacement,0)
-            listPlayers[1].set_image("./sprite/Joueur2/Mage2DR-1.png")
+        magic.launchBoule((0,-1),listSorts, player)
+
+def touchJ2(keys, player, listDecors, playerDeplacement, listSorts):
+    if(keys[K_UP]):
+        if(player.getJump() == 0 and player.getLanded() == 1):
+            player.setJump(1);
+            player.setAscend(1);
+            player.setAscendValue(10);
+            player.setDeplacement(0,-player.getAscendValue());
+    if(keys[K_DOWN]):
+        print '2bas';
+    if(keys[K_LEFT]):
+        if(physic.isTouching('left',player,listDecors) == 0):
+            player.setDeplacement(-playerDeplacement,0)
+            player.set_image("./sprite/Joueur2/Mage2GA-1.png")
+    if(keys[K_RIGHT]):
+        if(physic.isTouching('right',player,listDecors) == 0):
+            player.setDeplacement(playerDeplacement,0)
+            player.set_image("./sprite/Joueur2/Mage2DR-1.png")
+    if(keys[51] and keys[53]):
+        print("haut droit")
+        magic.launchBoule((1,-1),listSorts, player)
+    elif(keys[50] and keys[51]):
+        print("bas droit")
+        magic.launchBoule((1,1),listSorts, player)
+    elif(keys[51]):#droite
+        magic.launchBoule((1,0),listSorts, player)
+    elif(keys[49] and keys[50]):
+        print("bas gauche")
+        magic.launchBoule((-1,1),listSorts, player)
+    elif(keys[49] and keys[53]):
+        print("haut gauche")
+        magic.launchBoule((-1,-1),listSorts, player)
+    elif(keys[49]):#gauche
+        print("gauche")
+        magic.launchBoule((-1,0),listSorts, player)
+    elif(keys[50]):#bas
+        print("bas")
+        magic.launchBoule((0,1),listSorts, player)
+    elif(keys[53]):#haut
+        print("haut")
+        magic.launchBoule((0,-1),listSorts, player)
